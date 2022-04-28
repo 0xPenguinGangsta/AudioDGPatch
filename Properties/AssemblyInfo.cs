7bd5cb68aa59aa5d6eb38faa675209bbd13c140b
S00T4 <100884311+S00T4Dev@users.noreply.github.com>
2022-04-28 16:53:13 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AudioDG Patcher")]
+[assembly: AssemblyDescription("Patch Windows Audio Service for VoiceMeeter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AudioDG Patcher")]
+[assembly: AssemblyCopyright("Copyright © D0wzy 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d705eff1-83cd-4880-a1c5-86c431ee4a9c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
